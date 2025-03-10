c432fbc18ca441379cc7d6719a36b13fbdc9dfd0
Thomas Durand <durand.thomas765@gmail.com>
2025-03-10 11:12:33 +0100

    test final

--- next.config.js
+++ next.config.js
@@ -3,18 +3,10 @@ const nextConfig = {
   // Désactive ESLint pendant le build
   eslint: {
     ignoreDuringBuilds: true,
-    dirs: [], // Désactive complètement la vérification ESLint
   },
   // Désactive la vérification des types TypeScript pendant le build
   typescript: {
     ignoreBuildErrors: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/i,
-      use: ["style-loader", "css-loader", "postcss-loader"],
-    });
-    return config;
   },
 };
 
